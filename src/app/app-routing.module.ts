ab46a53e367bc1128013591ff096ca976013b4af
soumia <b.soumia50@gmail.com>
2021-11-19 17:09:45 +0100

    pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
+import { DetailComponent } from './detail/detail.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -9,8 +9,8 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'contact',
-    component: ContactComponent
+    path: 'detail/:id',
+    component: DetailComponent
   }
 ];
 
